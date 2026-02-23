59da260634259c8c2377bf95e252b19c71b7a245
Víctor <vjga2006@gmai.com>
2026-02-23 19:48:46 +0000

    metodo init agregado

--- src/main/java/mycode/Python_dict.java
+++ src/main/java/mycode/Python_dict.java
@@ -4,4 +4,12 @@ public class Python_dict {
     private Entry[] table;
     private int used;
     private int filled;
+
+    public Python_dict(int capacity){
+        table = new Entry[capacity];
+        used = 0;
+        filled = 0;
+    }
+
+
 }
