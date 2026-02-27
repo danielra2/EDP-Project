4f774e6e79e185693ffcbc930778fb4d72dc9549
Víctor <vjga2006@gmai.com>
2026-02-27 15:55:50 +0000

    Python_dict.java: new method put() -> allows to insert a new key-value pair in the dict new method resize() -> method that expands the size of the table correction on line 19

--- src/main/java/mycode/Python_dict.java
+++ src/main/java/mycode/Python_dict.java
@@ -1,22 +1,79 @@
 package mycode;
 
 public class Python_dict {
-    private Entry[] table;
-    private int used;
-    private int filled;
+    private Entry[] table;  //storage Array
+    private int used;       //number of used positions (not deleted)
+    private int filled;     //number of positions filled (deleted)
 
+    //constructor
     public Python_dict(int capacity){
         table = new Entry[capacity];
         used = 0;
         filled = 0;
     }
 
+    //converts the key into an array index
     private int index(String key){
-        int h = key.hashCode();
-        if(h==0){
+        int h = key.hashCode(); //obtain the hash code
+        //avoid negatives
+        if(h<0){
             h = -h;
         }
+        // size adjustment
         return h % table.length;
+    }
+    // method that expands the size of the table
+    private void resize(){
+        Entry[] oldTable = table;
+        table = new Entry[oldTable.length * 2];
+        used = 0;
+        filled = 0;
+
+        for(int i=0; i<oldTable.length; i++){
+            if(oldTable[i] != null && !oldTable[i].delete){
+                put(oldTable[i].key, oldTable[i].value);
+            }
+        }
+    }
+
+    //insert a new key value pair into the dict
+    public void put(String key, String value){
+        //if the table is more than 2/3 full, we make it bigger
+        if((double) filled / table.length > 0.66){
+            resize();
+        }
+
+        //first index
+        int hashIdx = index(key);
+        //current position
+        int pos = 0;
+
+        while(true){
+            int index = (hashIdx + pos * pos) % table.length;
+
+            // append a new key value pair if the pos is empty(null)
+            if(table[index] == null){
+                table[index] = new Entry(key, value);
+                used++;
+                filled++;
+                return;
+            }
+
+            // if pos was deleted, is reused
+            if(table[index].delete){
+                table[index] = new Entry(key, value);
+                used++;
+                return;
+            }
+
+            // if the key exists, update the value
+            if(table[index].key.equals(key)){
+                table[index].value = value;
+                return;
+            }
+            //incrementing position
+            pos++;
+        }
     }
 
 }
