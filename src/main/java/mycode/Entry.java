8a075a0d0d098ef422fad150776971af8507465a
iscab <iscabrera06@gmail.com>
2026-03-31 13:51:12 +0100

    JavaDoc PythonDict

--- src/main/java/mycode/Entry.java
+++ src/main/java/mycode/Entry.java
@@ -1,5 +1,16 @@
 package mycode;
 
+/**
+ * Clase que representa un nodo interno en la estructura del diccionario.
+ *
+ * @author Ismael Cabrera Cabrera
+ * @author Víctor Jesús García Abad
+ * @author Rodrigo Andrés Irigoyen Barrios
+ * @author Daniel Radoi
+ * @author Sara Trujillo García
+ * @param <K> El tipo de la clave almacenada.
+ * @param <V> El tipo del valor asociado.
+ */
 public class Entry<K, V> {
     K key;
     V value;
@@ -8,6 +19,12 @@ public class Entry<K, V> {
     Entry<K, V> prev;
     Entry<K, V> next;
 
+    /**
+     * Constructor para una nueva entrada. El estado inicial de borrado es siempre.
+     *
+     * @param key La clave del elemento.
+     * @param value El valor del elemento.
+     */
     public Entry(K key, V value){
         this.key = key;
         this.value = value;
