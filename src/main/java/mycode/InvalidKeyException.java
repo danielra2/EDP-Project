22f247299f5d7535521ba2daab50f3a3d387a49b
iscab <iscabrera06@gmail.com>
2026-03-31 13:47:42 +0100

    JavaDoc PythonDict

--- src/main/java/mycode/InvalidKeyException.java
+++ src/main/java/mycode/InvalidKeyException.java
@@ -1,6 +1,20 @@
 package mycode;
 
+/**
+ * Excepción lanzada cuando se intenta realizar una operación con una clave no válida.
+ *
+ * @author Ismael Cabrera Cabrera
+ * @author Víctor Jesús García Abad
+ * @author Rodrigo Andrés Irigoyen Barrios
+ * @author Daniel Radoi
+ * @author Sara Trujillo García
+ */
 public class InvalidKeyException extends RuntimeException {
+
+    /**
+     * Construye una nueva excepción con el mensaje de error especificado.
+     * @param message El detalle del motivo por el cual la clave es inválida.
+     */
     public InvalidKeyException(String message) {
         super(message);
     }
