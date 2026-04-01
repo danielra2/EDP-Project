c0eae021ce2b1724c7b7a4047fb1857e60c96704
iscab <iscabrera06@gmail.com>
2026-04-01 12:34:32 +0100

    JavaDoc

--- src/main/java/mycode/KeyError.java
+++ src/main/java/mycode/KeyError.java
@@ -1,9 +1,24 @@
 package mycode;
 
 /**
- * Custom exception used when a key is not found in the dictionary.
+ * Excepción que se lanza cuando se intenta acceder
+ * a una clave que no existe en el diccionario.
+ * Funciona de forma similar al KeyError de Python.
+ *
+ * @author Víctor Jesús García Abad
+ * @author Rodrigo Andrés Irigoyen Barrios
+ * @author Daniel Radoi
+ * @author Ismael Cabrera Cabrera
+ * @author Sara Trujillo García
  */
 public class KeyError extends RuntimeException {
+
+    /**
+     * Crea una nueva excepción indicando la clave
+     * que no se ha encontrado.
+     *
+     * @param key clave que no existe en el diccionario
+     */
     public KeyError(Object key) {
         super("Key not found: " + key);
     }
